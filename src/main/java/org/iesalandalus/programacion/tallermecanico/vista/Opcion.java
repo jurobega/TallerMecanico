7a245470d5ab48d244747efa1e19495fd1cbcd60
jurob <jurobega2@gmail.com>
2025-03-10 13:55:30 +0100

    Clase Opcion  terminada

--- src/main/java/org/iesalandalus/programacion/tallermecanico/vista/Opcion.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/vista/Opcion.java
@@ -6,25 +6,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum Opcion {
-    INSERTAR_CLIENTE(1,"Insertar Cliente"),
-    BUSCAR_CLIENTE(2,"Buscar Cliente"),
-    BORRAR_CLIENTE(3,"Borrar Cliente"),
-    LISTAR_CLIENTES(4,"Listar Cliente"),
-    MODIFICAR_CLIENTE(5,"Modificar Cliente"),
-    INSERTAR_VEHICULO(6,"Insertar Vehiculo"),
-    BUSCAR_VEHICULO(7,"Buscar Vehiculo"),
-    BORRAR_VEHICULO(8,"Borrar Vehiculo"),
-    LISTAR_VEHICULOS(9,"Listar Vehiculo"),
-    INSERTAR_REVISION(10,"Insertar Revision"),
-    BUSCAR_REVISION(11,"Buscar Revision"),
-    BORRAR_REVISION(12,"Borrar Revision"),
-    LISTAR_REVISIONES(13,"Listar Revisiones"),
-    LISTAR_REVISIONES_CLIENTE(14,"Listar Revisiones Cliente"),
-    LISTAR_REVISIONES_VEHICULO(15,"Listar Revisiones Vehiculo"),
-    ANADIR_HORAS_REVISION(16,"Añadir Horas Revision"),
-    ANADIR_PRECIO_MATERIAL_REVISION(17,"Añadir Precio Material Revision"),
-    CERRAR_REVISION(18,"Cerrar Revision"),
-    SALIR(19,"Salir");
+    INSERTAR_CLIENTE(10,"Insertar Cliente"),
+    BUSCAR_CLIENTE(11,"Buscar Cliente"),
+    BORRAR_CLIENTE(12,"Borrar Cliente"),
+    LISTAR_CLIENTES(13,"Listar Cliente"),
+    MODIFICAR_CLIENTE(14,"Modificar Cliente"),
+    INSERTAR_VEHICULO(20,"Insertar Vehiculo"),
+    BUSCAR_VEHICULO(21,"Buscar Vehiculo"),
+    BORRAR_VEHICULO(22,"Borrar Vehiculo"),
+    LISTAR_VEHICULOS(23,"Listar Vehiculo"),
+    INSERTAR_REVISION(30,"Insertar Revision"),
+    BUSCAR_REVISION(31,"Buscar Revision"),
+    BORRAR_REVISION(32,"Borrar Revision"),
+    LISTAR_REVISIONES(33,"Listar Revisiones"),
+    LISTAR_REVISIONES_CLIENTE(34,"Listar Revisiones Cliente"),
+    LISTAR_REVISIONES_VEHICULO(35,"Listar Revisiones Vehiculo"),
+    ANADIR_HORAS_REVISION(40,"Añadir Horas Revision"),
+    ANADIR_PRECIO_MATERIAL_REVISION(50,"Añadir Precio Material Revision"),
+    CERRAR_REVISION(60,"Cerrar Revision"),
+    SALIR(0,"Salir");
 
     private int numeroOpcion;
     private String mensaje;
