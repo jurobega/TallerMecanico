dfa5ae8b1a7df2be29cfda45f40e3c29e0f9ebc9
jurob <jurobega2@gmail.com>
2025-03-19 12:14:20 +0100

    Clase main  terminada

--- src/main/java/org/iesalandalus/programacion/tallermecanico/Main.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/Main.java
@@ -1,7 +1,15 @@
-package org.iesalandalus.programacion.tallermecanico;
+    package org.iesalandalus.programacion.tallermecanico;
 
-public class Main {
-    public static void main(String[] args) {
+    import org.iesalandalus.programacion.tallermecanico.controlador.Controlador;
+    import org.iesalandalus.programacion.tallermecanico.modelo.Modelo;
+    import org.iesalandalus.programacion.tallermecanico.modelo.TallerMecanicoExcepcion;
+    import org.iesalandalus.programacion.tallermecanico.vista.Vista;
 
+    public class Main {
+        public static void main(String[] args) throws TallerMecanicoExcepcion {
+            Modelo modelo = new Modelo();
+            Vista vista = new Vista();
+            Controlador controlador = new Controlador(modelo , vista);
+            controlador.comenzar();
+        }
     }
-}
